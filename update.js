13056b730ba20f18aef3aeca67c75cd8276306f4
mrepol742 <mrepol742@gmail.com>
2023-01-02 06:25:44 +0000

    Initial Commit

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 let applicationId = "com.mrepol742.webvium.beta";
 let versionName = 28;
 let versionFullName = "v2.8";
-let versionCode = 22122606;
-let latestRelease = "December 26, 2022"
+let versionCode = 23010206;
+let latestRelease = "January 02, 2023"
