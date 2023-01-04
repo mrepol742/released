99928ae2e30a2f6aa9a934e6af3795c776a3c2d1
mrepol742 <mrepol742@gmail.com>
2023-01-04 06:13:12 +0000

    Initial Commit

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 let applicationId = "com.mrepol742.webvium.dev";
 let versionName = 28;
 let versionFullName = "v2.8";
-let versionCode = 22120702;
-let latestRelease = "December 07, 2022"
+let versionCode = 23010406;
+let latestRelease = "January 04, 2023"
